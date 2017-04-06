1dd02f42aa0e8da36884a3a27a6bb20f768b39fc
Nikhil Narang <nikhilnarang@Nikhils-MacBook.local>
2017-04-06 15:34:46 -0700

    converted all space indentations to tabs

--- src/check.py
+++ src/check.py
@@ -6,43 +6,43 @@ from pprint import pprint
 check_types = ['compatibility', 'consistency']
 
 class Check(object):
-    """Check class provides methods for pre-defined check types
+	"""Check class provides methods for pre-defined check types
 
-    Attributes:
-      check: a string type associated with a check
-      contracts: a list of contracts associated with a check
+	Attributes:
+		check: a string type associated with a check
+		contracts: a list of contracts associated with a check
 
-    """
-    def __init__(self):
-        """Initialize a check object"""
-        self.check_type = ''
-        self.contracts = []
+	"""
+	def __init__(self):
+		"""Initialize a check object"""
+		self.check_type = ''
+		self.contracts = []
 
-    def set_type(self, check_type):
-        """Assigns the check type"""
-        self.check_type = [x for x in check_types if x == check_type][0]
+	def set_type(self, check_type):
+		"""Assigns the check type"""
+		self.check_type = [x for x in check_types if x == check_type][0]
 
-    def set_contracts(self, contracts):
-        """Assigns the contracts associated with the check"""
-        self.contracts = contracts
+	def set_contracts(self, contracts):
+		"""Assigns the contracts associated with the check"""
+		self.contracts = contracts
 
-    def __str__(self):
-        """Override the print behavior"""
-        astr = 'Check: ' + self.check_type + '\n'
-        astr += 'Contracts:\n'
-        for contract in self.contracts:
-            astr += ('  ' + contract.name + '\n')
-        return astr
+	def __str__(self):
+		"""Override the print behavior"""
+		astr = 'Check: ' + self.check_type + '\n'
+		astr += 'Contracts:\n'
+		for contract in self.contracts:
+			astr += ('  ' + contract.name + '\n')
+		return astr
 
-    def __eq__(self, other):
-        """Override the default Equals behavior"""
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        return False
+	def __eq__(self, other):
+		"""Override the default Equals behavior"""
+		if isinstance(other, self.__class__):
+			return self.__dict__ == other.__dict__
+		return False
 
-    def __ne__(self, other):
-        """Define a non-equality test"""
-        return not self.__eq__(other)
+	def __ne__(self, other):
+		"""Define a non-equality test"""
+		return not self.__eq__(other)
 
 class Checks(object):
 	def __init__(self):
--- tests/test_library.py
+++ tests/test_library.py
@@ -10,55 +10,55 @@ from src.check import Checks
 sys.path.append(os.path.join(os.getcwd(), os.path.pardir))
 
 class TestLibrary(unittest.TestCase):
-    """TestLibrary class contains method to test LTL contract verifier operations"""
+	"""TestLibrary class contains method to test LTL contract verifier operations"""
 
-    def test_waiter_customer_model(self):
-        """Parse waiter customer model and verify output"""
+	def test_waiter_customer_model(self):
+		"""Parse waiter customer model and verify output"""
 
-        # parse waiter customer model
-        parser_obj = Parser('tests/waiter_customer.txt')
-        contracts, checks = parser_obj.parse()
+		# parse waiter customer model
+		parser_obj = Parser('tests/waiter_customer.txt')
+		contracts, checks = parser_obj.parse()
 
-        # verify all contracts have been parsed
-        self.assertTrue('waiter' in contracts)
-        self.assertTrue('customer' in contracts)
-        self.assertEqual(2, len(contracts))
+		# verify all contracts have been parsed
+		self.assertTrue('waiter' in contracts)
+		self.assertTrue('customer' in contracts)
+		self.assertEqual(2, len(contracts))
 
-        # verify waiter contract parsed correctly
-        waiter = Contract()
-        waiter.set_name(['waiter'])
-        waiter.set_variables(['request := FALSE', 'service := FALSE'])
-        waiter.set_assumptions(['TRUE'])
-        waiter.set_guarantees(['G(!request -> X !service)', 'G(request -> X service)'])
-        self.assertEqual(contracts['waiter'], waiter)
+		# verify waiter contract parsed correctly
+		waiter = Contract()
+		waiter.set_name(['waiter'])
+		waiter.set_variables(['request := FALSE', 'service := FALSE'])
+		waiter.set_assumptions(['TRUE'])
+		waiter.set_guarantees(['G(!request -> X !service)', 'G(request -> X service)'])
+		self.assertEqual(contracts['waiter'], waiter)
 
-        # verify customer contract parsed correctly
-        customer = Contract()
-        customer.set_name(['customer'])
-        customer.set_variables(['request := FALSE', 'service := FALSE'])
-        customer.set_assumptions(['TRUE'])
-        customer.set_guarantees(['(F request)', 'G((request & !service) -> X request)',
-                                 'G(service -> X !request)'])
-        self.assertEqual(contracts['customer'], customer)
+		# verify customer contract parsed correctly
+		customer = Contract()
+		customer.set_name(['customer'])
+		customer.set_variables(['request := FALSE', 'service := FALSE'])
+		customer.set_assumptions(['TRUE'])
+		customer.set_guarantees(['(F request)', 'G((request & !service) -> X request)',
+								 'G(service -> X !request)'])
+		self.assertEqual(contracts['customer'], customer)
 
-        # verify all checks have been parsed correctly
-        self.assertTrue('compatibility' in checks)
-        self.assertTrue('consistency' in checks)
-        self.assertEqual(2, len(checks))
+		# verify all checks have been parsed correctly
+		self.assertTrue('compatibility' in checks)
+		self.assertTrue('consistency' in checks)
+		self.assertEqual(2, len(checks))
 
-        # verify compatibility check
-        compatibility = Check()
-        compatibility.set_type('compatibility')
-        compatibility.set_contracts([waiter, customer])
-        self.assertEqual(compatibility, checks['compatibility'])
+		# verify compatibility check
+		compatibility = Check()
+		compatibility.set_type('compatibility')
+		compatibility.set_contracts([waiter, customer])
+		self.assertEqual(compatibility, checks['compatibility'])
 
-        # verify consistency check
-        consistency = Check()
-        consistency.set_type('consistency')
-        consistency.set_contracts([waiter, customer])
-        self.assertEqual(consistency, checks['consistency'])
+		# verify consistency check
+		consistency = Check()
+		consistency.set_type('consistency')
+		consistency.set_contracts([waiter, customer])
+		self.assertEqual(consistency, checks['consistency'])
 
-    def test_checks(self):
-        """Runs the program and outputs status of the LTL statements"""
-        checks = Checks()
-        checks.run()
+	def test_checks(self):
+		"""Runs the program and outputs status of the LTL statements"""
+		checks = Checks()
+		checks.run()
