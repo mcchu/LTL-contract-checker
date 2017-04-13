b0603012486cca1054cf175bf2aa0808e4ff44c0
Jack Kellner <jack@kellner.com>
2017-04-12 22:50:02 -0700

    Added inital functions for composition, conjunction, and refinement.

--- src/check.py
+++ src/check.py
@@ -2,6 +2,7 @@
 """Check module defines a check class that links contracts to a pre-defined check type"""
 import subprocess
 from pprint import pprint
+from contract import Contract
 
 check_types = ['compatibility', 'consistency']
 
@@ -93,6 +94,12 @@ class Checks(object):
             if check.check_type == 'compatibility': 
                 comp = self.compatibility(contracts[check.contract_names[0]],contracts[check.contract_names[1]])
                 outfile.write(comp)
+
+                # Uncomment the line below if you want to test the correctness of the composition function)
+                # self.composition(contracts[check.contract_names[0]],contracts[check.contract_names[1]])
+
+                # Uncomment the line below if you want to test the correctness of the conjunction function)
+                # self.conjunction(contracts[check.contract_names[0]],contracts[check.contract_names[1]])
             elif check.check_type == 'consistency': 
                 const = self.consistency(contracts[check.contract_names[0]],contracts[check.contract_names[1]])
                 outfile.write(const)
@@ -111,7 +118,38 @@ class Checks(object):
         contract_a and contract_b should both be contract objects"""
         cons = "\tLTLSPEC !(" + contract_a.guarantees + " & " + contract_b.guarantees + ");\n"
         return cons
-        
+
+    def refinement(self, contract_a, contract_b):
+        """Returns the 2 LTL expression that should be used to check if contract_a refines contract_b
+        NOTE: The order of contract_a and contract_b is important as refinement is not always reversible.
+         
+        Contract_a is a refinement of contract_b iff the inverse of the below LTL expressions are BOTH unsatisfiable"""
+        ltl = []
+        ltl.append("( (" + contract_b.assumptions + ") -> (" + contract_a.assumptions+ ") )")
+        ltl.append("( (" + contract_a.guarantees + ") -> (" + contract_b.guarantees+ ") )")
+        pprint(ltl)
+        return ltl
+
+    def composition(self, contract_a, contract_b):
+        """Returns a new contract that is the composition of contract_a and contract_b"""
+        comp = Contract()
+        comp.set_name([contract_a.name + "_comp_" + contract_b.name])
+        comp.set_variables(contract_a.variables)
+        comp.set_assumptions("(" + contract_a.assumptions + " & " + contract_b.assumptions + ") | !(" + contract_a.guarantees + " & " + contract_b.guarantees + ")")
+        comp.set_guarantees("(" + contract_a.guarantees + " & " + contract_b.guarantees + ")")
+        print comp
+        return comp
+    
+    def conjunction(self, contract_a, contract_b):
+        """Returns a new contract that is the conjunction of contract_a and contract_b"""
+        conj = Contract()
+        conj.set_name([contract_a.name + "_conj_" + contract_b.name])
+        conj.set_variables(contract_a.variables)
+        conj.set_assumptions("(" + contract_a.assumptions + " | " + contract_b.assumptions + ")")
+        conj.set_guarantees("(" + contract_a.guarantees + " & " + contract_b.guarantees + ")")
+        print conj
+        return conj
+
     def run(self, contracts):
         """runs the set of contracts and checks through NuSMV and parses the results to return to the user"""
 
